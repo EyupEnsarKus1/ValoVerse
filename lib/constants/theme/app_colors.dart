0fe56dcd844b04650f34da8418e4acc3dfa63a66
İsmail Çakıcı <ismailcakici30@gmail.com>
2023-06-14 23:56:23 +0300

    -project structure created, colors and assets added to project
    - first version of project structre created
    - fonts, icon and images added to assets
    - bloc package added to pubspec.yaml
    - app colors created

---
+++ lib/constants/theme/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+  static const solidBlack = Color(0xff111111);
+  static const solidWhite = Color(0xffECE8E1);
+  static const solidRed = Color(0xffFF4655);
+  static const darkBlue = Color(0xff0F1923);
+}
