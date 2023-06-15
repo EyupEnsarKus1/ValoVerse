07878f6445ffc74f6aa77a6c0205000678e930ee
EyupEnsarKus1 <eyup@finfree.co>
2023-06-15 23:13:56 +0300

    app colors added

--- lib/constants/theme/app_colors.dart
+++ lib/constants/theme/app_colors.dart
@@ -2,8 +2,25 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   AppColors._();
-  static const solidBlack = Color(0xff111111);
-  static const solidWhite = Color(0xffECE8E1);
-  static const solidRed = Color(0xffFF4655);
-  static const darkBlue = Color(0xff0F1923);
+
+  // Main Colors
+
+  static const black = Color(0xff111111);
+  static const white = Color(0xffECE8E1);
+  static const red = Color(0xffFF4655);
+  static const blue = Color(0xff0F1923);
+
+  // Light Colors
+
+  static const lightBlackLevel2 = Color(0xff333333);
+  static const lightBlackLevel3 = Color(0xff555555);
+  static const lightBlackLevel4 = Color(0xff777777);
+
+  static const lightRedLevel2 = Color(0xffFF6A70);
+  static const lightRedLevel3 = Color(0xffFF8E8B);
+  static const lightRedLevel4 = Color(0xffFFB2A6);
+
+  static const lightBlueLevel2 = Color(0xff33414B);
+  static const lightBlueLevel3 = Color(0xff576973);
+  static const lightBlueLevel4 = Color(0xff7B919B);
 }
