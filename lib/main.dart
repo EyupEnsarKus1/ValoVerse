632c2351678ab02f5ea64ab65bf76f619b32593a
İsmail Çakıcı <ismailcakici30@gmail.com>
2023-07-03 13:04:38 +0300

    - localization cubit added

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_vector_icons/flutter_vector_icons.dart';
+import 'package:valorant_wiki_app/bloc/lang_cubit/lang_cubit.dart';
+import 'package:valorant_wiki_app/bloc/theme_cubit/theme_cubit.dart';
 import 'package:valorant_wiki_app/ui/constants/colors/app_colors.dart';
 import 'package:valorant_wiki_app/ui/constants/enums/fonts_enum.dart';
+import 'package:valorant_wiki_app/ui/constants/extensions/string_extension.dart';
 import 'package:valorant_wiki_app/ui/constants/localization/localization_constants.dart';
 import 'package:valorant_wiki_app/ui/pages/agentsPage.dart';
 
-import 'bloc/theme_cubit/theme_cubit.dart';
+import 'core/locale_keys.g.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,97 +20,145 @@ void main() async {
     EasyLocalization(
       path: LocalizationConstants.path,
       supportedLocales: LocalizationConstants.supportedLocales,
+      fallbackLocale: LocalizationConstants.enLocale,
       child: const MyApp(),
     ),
   );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
         BlocProvider<ThemeCubit>(create: (_) => ThemeCubit()),
+        BlocProvider<LangCubit>(
+          create: (_) => LangCubit(context: context),
+        ),
       ],
-      child: BlocBuilder<ThemeCubit, ThemeState>(builder: (context, state) {
-        return MaterialApp(
-          builder: (context, child) {
-            final mediaQueryData = MediaQuery.of(context);
-            ScreenUtil.init(context);
-            AppSizes.init();
-            return MediaQuery(
-              data: mediaQueryData,
-              child: child!,
-            );
-          },
-          title: 'ValorantWikiApp',
-          debugShowCheckedModeBanner: false,
-          theme: state.themeData,
-          locale: context.locale,
-          supportedLocales: context.supportedLocales,
-          localizationsDelegates: context.localizationDelegates,
-          home: HomePage(),
-        );
-      }),
+      child: BlocBuilder<LangCubit, LangState>(
+        builder: (context, langState) {
+          return BlocBuilder<ThemeCubit, ThemeState>(
+            builder: (context, themeState) {
+              return ScreenUtilInit(
+                builder: (context, child) {
+                  return MaterialApp(
+                    title: 'ValorantWikiApp',
+                    debugShowCheckedModeBanner: false,
+                    theme: themeState.themeData,
+                    locale: langState.locale,
+                    supportedLocales: context.supportedLocales,
+                    localizationsDelegates: context.localizationDelegates,
+                    home: HomePage(),
+                  );
+                },
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    final langCubit = BlocProvider.of<LangCubit>(context);
+    final themeCubit = BlocProvider.of<ThemeCubit>(context);
+    langCubit.setTurkish();
+    themeCubit.setTheme();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return BlocBuilder<ThemeCubit, ThemeState>(builder: (context, state) {
-      final themeCubit = context.read<ThemeCubit>();
-      return SafeArea(
-        child: Scaffold(
-          appBar: AppBar(
-            actions: [
-              IconButton(
-                onPressed: () {
-                  themeCubit.setTheme();
-                },
-                icon: Icon(
-                  MaterialIcons.brightness_3,
-                  color: Theme.of(context).brightness == Brightness.light
-                      ? AppColors.blue
-                      : AppColors.primaryColor,
-                ),
-              ),
-            ],
-            centerTitle: true,
-            title: Text(
-              "Valorant",
-              style: TextStyle(
-                color: Theme.of(context).brightness == Brightness.light
-                    ? AppColors.blue
-                    : AppColors.primaryColor,
-                fontSize: AppSizes.headlineMedium,
-                fontWeight: AppWeights.veryBold,
-                fontFamily: AppFonts.valorant,
-              ),
-            ),
-          ),
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          actions: [
+            Row(
               children: [
-                ElevatedButton(
+                IconButton(
                   onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => AgentsPage()));
+                    final langCubit = BlocProvider.of<LangCubit>(context);
+                    langCubit.setTurkish();
                   },
-                  child: Text(""),
+                  icon: Text('tr'),
+                ),
+                IconButton(
+                  onPressed: () {
+                    final langCubit = BlocProvider.of<LangCubit>(context);
+                    langCubit.setEnglish();
+                  },
+                  icon: Text('en'),
                 ),
               ],
             ),
+            IconButton(
+              onPressed: () {
+                final themeCubit = BlocProvider.of<ThemeCubit>(context);
+                themeCubit.setTheme();
+              },
+              icon: Icon(
+                MaterialIcons.brightness_3,
+                color: BlocProvider.of<ThemeCubit>(context)
+                            .state
+                            .themeData
+                            .brightness ==
+                        Brightness.light
+                    ? AppColors.blue
+                    : AppColors.primaryColor,
+              ),
+            ),
+          ],
+          centerTitle: true,
+          title: Text(
+            "Valorant",
+            style: TextStyle(
+              color: BlocProvider.of<ThemeCubit>(context)
+                          .state
+                          .themeData
+                          .brightness ==
+                      Brightness.light
+                  ? AppColors.blue
+                  : AppColors.primaryColor,
+              fontSize: AppSizes.headlineMedium,
+              fontWeight: AppWeights.veryBold,
+              fontFamily: AppFonts.valorant,
+            ),
           ),
         ),
-      );
-    });
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(LocaleKeys.agents_abilities.translate),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => AgentsPage(),
+                    ),
+                  );
+                },
+                child: Text(""),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
