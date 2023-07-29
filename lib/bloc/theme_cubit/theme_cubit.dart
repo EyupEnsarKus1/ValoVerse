58d66f4b1c324252467dd66bfc8c88714f90a09a
EyupEnsarKus1 <eyup@finfree.co>
2023-07-30 01:21:48 +0300

    sharedPrefrences ımplemented

--- lib/bloc/theme_cubit/theme_cubit.dart
+++ lib/bloc/theme_cubit/theme_cubit.dart
@@ -1,22 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:valorant_wiki_app/ui/constants/theme/dark_theme.dart';
-import 'package:valorant_wiki_app/ui/constants/theme/light_theme.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../ui/constants/theme/dark_theme.dart';
+import '../../ui/constants/theme/light_theme.dart';
 
 class ThemeCubit extends Cubit<ThemeState> {
-  ThemeCubit()
-      : super(ThemeState(themeData: AppDarkTheme.darkTheme, isLight: false));
+  SharedPreferences? prefs;
 
-  void setTheme() {
-    if (state.themeData == AppLightTheme.lightTheme) {
-      final updateState =
-          ThemeState(themeData: AppDarkTheme.darkTheme, isLight: false);
-      emit(updateState);
-    } else {
-      final updateState =
-          ThemeState(themeData: AppLightTheme.lightTheme, isLight: true);
-      emit(updateState);
-    }
+  ThemeCubit() : super(ThemeState(themeData: AppDarkTheme.darkTheme, isLight: false)) {
+    loadTheme();
+  }
+
+  Future<void> loadTheme() async {
+    prefs = await SharedPreferences.getInstance();
+    bool isLight = prefs!.getBool('isLight') ?? false;
+    final themeData = isLight ? AppLightTheme.lightTheme : AppDarkTheme.darkTheme;
+    emit(ThemeState(themeData: themeData, isLight: isLight));
+  }
+
+  Future<void> setTheme(bool newTheme) async {
+    prefs = await SharedPreferences.getInstance();
+    final themeData = newTheme ? AppLightTheme.lightTheme : AppDarkTheme.darkTheme;
+    await _saveTheme(newTheme);
+    emit(ThemeState(themeData: themeData, isLight: newTheme));
+  }
+
+  Future<void> _saveTheme(bool isLight) async {
+    prefs = await SharedPreferences.getInstance();
+    await prefs!.setBool('isLight', isLight);
   }
 }
 
