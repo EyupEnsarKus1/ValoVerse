4c2c2e0ccf6b847b27f6bdffef75ba6b6fe19f60
İsmail Çakıcı <ismailcakici30@gmail.com>
2023-07-30 22:32:29 +0300

    dropdown updated on setting page

--- lib/ui/pages/settings_page/settings_page.dart
+++ lib/ui/pages/settings_page/settings_page.dart
@@ -4,8 +4,12 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:valorant_wiki_app/bloc/lang_cubit/lang_cubit.dart';
 import 'package:valorant_wiki_app/bloc/theme_cubit/theme_cubit.dart';
 import 'package:valorant_wiki_app/core/locale_keys.g.dart';
+import 'package:valorant_wiki_app/ui/constants/colors/app_colors.dart';
+import 'package:valorant_wiki_app/ui/constants/enums/radius_enum.dart';
+import 'package:valorant_wiki_app/ui/constants/extensions/radius_extension.dart';
 import 'package:valorant_wiki_app/ui/constants/extensions/string_extension.dart';
 import 'package:valorant_wiki_app/ui/constants/localization/localization_constants.dart';
+import 'package:valorant_wiki_app/ui/constants/styles/fonts.dart';
 import 'package:valorant_wiki_app/ui/custom_widgets/custom_appBar.dart';
 import 'package:valorant_wiki_app/ui/pages/settings_page/settings_page_card.dart';
 
@@ -15,7 +19,10 @@ class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppBar(title: LocaleKeys.settings.translate, backgroundColor: Colors.transparent, showBackButton: true),
+      appBar: CustomAppBar(
+          title: LocaleKeys.settings.translate,
+          backgroundColor: Colors.transparent,
+          showBackButton: true),
       body: SingleChildScrollView(
         physics: const BouncingScrollPhysics(),
         child: Column(
@@ -40,6 +47,12 @@ class SettingsPage extends StatelessWidget {
                   title: LocaleKeys.language.translate,
                   child: DropdownButton(
                     value: context.locale,
+                    borderRadius: RadiusEnum.normal.borderRadiusAll(),
+                    underline: const SizedBox.shrink(),
+                    style: TextStyle(
+                      fontSize: AppSizes.size12,
+                      fontFamily: AppFonts.archivo,
+                    ),
                     items: [
                       DropdownMenuItem(
                         value: LocalizationConstants.trLocale,
